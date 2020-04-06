e7a01a4749550575db1494b64b69c309189c97d2
Dmitry.QD <ilita.fund@gmail.com>
2020-04-06 22:20:27 +0100

    Typo fix

--- src/test/java/com/binance/api/client/domain/account/NewOrderResponseTest.java
+++ src/test/java/com/binance/api/client/domain/account/NewOrderResponseTest.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.containsString;
-// import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
